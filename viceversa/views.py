caacb21ae9ded4fa641f7f8b6f593c7bf2ea9151
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2021-03-06 06:43:22 +0700

    Add reverse featured

--- viceversa/views.py
+++ viceversa/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html', {'name': 'Vice Versa'})
+
 def reverse(request):
-	return render(request, 'reverse.html')
+	user_text = request.GET['usertext']
+	reversed_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
