b4cfd8f05c492b40a8e02a854cb08f55651620e6
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2021-03-06 06:50:46 +0700

    Count words in usertext

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,4 +7,8 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+
+	user_text_length = len(user_text.split())
+
+	return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'usertext_length': user_text_length})
+	
